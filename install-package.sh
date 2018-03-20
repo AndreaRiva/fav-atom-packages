33b90c6d1c8d610657c894067f83bbd791fe7304
Andrea Rivadossi <riva.andrea.web@gmail.com>
2018-03-20 08:50:41 +0100

    Update install-package.sh

--- install-package.sh
+++ install-package.sh
@@ -1,1 +1,10 @@
 apm install autoclose-html
+apm install highlight-selected
+apm install linter
+apm install linter-htmlhint
+apm install linter-jshint
+apm install linter-php
+apm install minimap
+apm install remote-ftp
+apm install split-diff
+apm install file-icons
