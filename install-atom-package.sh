16cabbd699da933f68f856b8ed97c91a2f6ac859
Andrea Rivadossi <riva.andrea.web@gmail.com>
2018-06-28 15:40:00 +0200

    Aggiunto installer veloce

---
+++ install-atom-package.sh
@@ -0,0 +1,11 @@
+apm install autoclose-html
+apm install highlight-selected
+apm install linter
+apm install linter-htmlhint
+apm install linter-jshint
+apm install linter-php
+apm install linter-markdown
+apm install minimap
+apm install remote-ftp
+apm install split-diff
+apm install file-icons
